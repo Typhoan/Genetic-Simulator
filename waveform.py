502c0e3bfccf3e42f0c98fc4d48965a13e0ea261
Alexander <blockhead54@users.noreply.github.com>
2015-02-19 22:21:28 -0600

    Update waveform.py
    changed zeroes to small random numbers for added authenticity
    (also added matplotlib functions to driver testing code - matplotlib doesn't come with standard python)

--- waveform.py
+++ waveform.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 def generateWaveform(sequence):
 	random.seed()
@@ -9,31 +10,34 @@ def generateWaveform(sequence):
 	for nucleotide in sequence:
 		if nucleotide is "A":
 			waveA.extend([0.2, 0.45, 0.7, 1 + ((random.random() - 0.5)/4), 0.7, 0.45, 0.2])
-			waveT.extend([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-			waveG.extend([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-			waveC.extend([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+			waveT.extend([(random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10)])
+			waveG.extend([(random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10)])
+			waveC.extend([(random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10)])
 		if nucleotide is "T":
-			waveA.extend([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+			waveA.extend([(random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10)])
 			waveT.extend([0.2, 0.45, 0.7, 1 + ((random.random() - 0.5)/4), 0.7, 0.45, 0.2])
-			waveG.extend([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-			waveC.extend([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+			waveG.extend([(random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10)])
+			waveC.extend([(random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10)])
 		if nucleotide is "G":
-			waveA.extend([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-			waveT.extend([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+			waveA.extend([(random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10)])
+			waveT.extend([(random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10)])
 			waveG.extend([0.2, 0.45, 0.7, 1 + ((random.random() - 0.5)/4), 0.7, 0.45, 0.2])
-			waveC.extend([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+			waveC.extend([(random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10)])
 		if nucleotide is "C":
-			waveA.extend([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-			waveT.extend([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-			waveG.extend([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+			waveA.extend([(random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10)])
+			waveT.extend([(random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10)])
+			waveG.extend([(random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10), (random.random()/10)])
 			waveC.extend([0.2, 0.45, 0.7, 1 + ((random.random() - 0.5)/4), 0.7, 0.45, 0.2])
 	return [waveA, waveT, waveG, waveC]
 	
-seq = "ATCAGGCGAGAGA"
+seq = "ATCAGGCGAGAGAGTGCTGATTAGGCGTCTCTCTAGATCGGGGGGCTTATATGCTTTTATGAGATCATATAGCTA"
 wave = generateWaveform(seq)
 import sys
 for i in range(0, len(wave)):
-	for j in range(0, 20):
-		sys.stdout.write(str(round(wave[i][j], 1)) + " "),
-	print ''
+	plt.plot(wave[i])
+plt.show()
+
+
+
+
 	
