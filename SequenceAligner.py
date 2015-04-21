698397949085a28060e636111a8ac52a928c2058
Alexander <blockhead54@users.noreply.github.com>
2015-04-20 19:53:40 -0500

    Update SequenceAligner.py
    changed alignment and dot matrix to use sequence names via dict objects

--- SequenceAligner.py
+++ SequenceAligner.py
@@ -67,51 +67,62 @@ class SequenceAligner:
     '''
     gives a dot matrix (alignment with similar nucleotides replaced with dots)
     
-    @param dominantAlignedSequence - the dominant sequence from the aligned sequence list
-    @param subdominantAlignedSequences - the subdominant aligned sequences
-    @return dotMatrix - the dot matrix
+    @param dominantName - the name of the dominant sequence
+    @param alignedSequences - the aligned sequences as a name->seq dict
+    @return dotMatrix - the dot matrix as a name->seq dict
     '''
-    def generateDotMatrix(self, dominantAlignedSequence, subdominantAlignedSequences):
-        dotMatrix = []
-        for sequence in subdominantAlignedSequences:
-            listCopy = list(sequence)
-            for i in range(len(sequence)):
-                if sequence[i] is not "-" and dominantAlignedSequence[i] is not "-":
-                    if sequence[i] == dominantAlignedSequence[i]:
-                        listCopy[i] = "."
-            dotMatrix.append(''.join(listCopy))
-        dotMatrix.insert(0, dominantAlignedSequence)
+    def generateDotMatrix(self, domName, alignedSeqs):
+        dotMatrix = dict()
+
+        #get copy of the alignment and remove dominant sequence from it
+        alignedCopy = dict(alignedSeqs)
+        del alignedCopy[domName]
+        domSeq = alignedSeqs[domName]
+
+        #get the dotted string for each subdominant sequence and add it to the dot matrix dict by name
+        for name, seq in alignedCopy.iteritems():
+            seqList = list(seq)
+            for i in range(len(seq)):
+                if seqList[i] != "-" and domSeq[i] != "-" and seqList[i] == domSeq[i]:
+                    seqList[i] = "."
+            dotMatrix[name] = ''.join(seqList)
+
+        #be sure dominant is in dot matrix and return
+        dotMatrix[domName] = domSeq
         return dotMatrix
+
 
     '''
     performs a multiple alignment on a list of sequences using Clustal Omega
 
-    @param seqMatrix - the sequence matrix containing all of the sequences to be aligned
-    @return returns the aligned sequences as a 2d list with gaps
+    @param seqMatrix - the sequence matrix (a name->seq dict) containing all of the sequences to be aligned
+    @return returns the aligned sequences as a name->seq dict with gaps
     '''
     def alignSequences(self, seqMatrix):
         fastaIn = tempfile.NamedTemporaryFile()
         fastaOut = tempfile.NamedTemporaryFile()
         with tempfile.NamedTemporaryFile() as fastaIn, tempfile.NamedTemporaryFile() as fastaOut:
-            for i in range(len(seqMatrix)):
-                fastaIn.write(">" + str(i) + "\n")
-                fastaIn.write(seqMatrix[i] + "\n")
+            for name, seq in seqMatrix.iteritems():
+                fastaIn.write(">" + name + "\n")
+                fastaIn.write(seq + "\n")
             fastaIn.seek(0)
             output = check_output(["clustalo", "-i", fastaIn.name, "--outfmt=fasta"])
             fastaOut.write(output)
             fastaOut.seek(0)
             dout = DNAFileDict.DNAFileDict(fastaOut.name)
             dout.setLists()
-            return dout.getDNADict().values()
+            return dout.getDNADict()
            
 '''
-sample = []
-sample.append("AGCTAGATTATAATTTCGGGCGATAGCTAGATCGATAGC")
-sample.append("GCTAGCCCCCTTTATAGGCTATATATAGATATCGCTGCT")
-sample.append("AGGGCTATCATCGACGTATCATATATACGCGATCGGTTA")
+sample = dict()
+sample["s1"] = "TTTTTTTT"
+sample["s2"] = "TTTTTTTTTTT"
+sample["s3"] = "TTTTTTGGGTT"
 a = SequenceAligner()
 seqs = a.alignSequences(sample)
-for s in seqs: print s
+for k, v in seqs.iteritems(): print k, v
 print ""
-for s in a.generateDotMatrix(seqs.pop(), seqs): print s
+seqs = a.generateDotMatrix("s1", seqs)
+for k, v in seqs.iteritems(): print k, v
 '''
+
